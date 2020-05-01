b06d5f8475a73a10ece6c363694ddf19484b5f86
Stefano Magni <nori.ste.magni@gmail.com>
2020-05-01 16:42:57 +0200

    Add little improvements

--- cypress/integration/talk/1-everything-works-in-the-browser.test.js
+++ cypress/integration/talk/1-everything-works-in-the-browser.test.js
@@ -4,16 +4,8 @@ context('Talk', () => {
   it('Logs everything in the console', () => {
     cy.visit('/')
     cy.log('cy.log from the test')
-    cy.log('cy.log an array', [1, 2, 3])
-    cy.log('cy.log an object', { foo: 'foo', bar: 'bar', baz: 'baz' })
 
     console.log('%c  console.log from the test ', 'background: #08BE8A; color: #FFF')
-    console.log('%c  console.log from the test ', 'background: #08BE8A; color: #FFF', [1, 2, 3])
-    console.log('%c  console.log from the test ', 'background: #08BE8A; color: #FFF', {
-      foo: 'foo',
-      bar: 'bar',
-      baz: 'baz',
-    })
 
     cy.window().then((win) => {
       console.log(
@@ -23,7 +15,6 @@ context('Talk', () => {
       )
     })
 
-    cy.log('Clicking on an assertion logs the result in console')
     cy.wrap({ foo: 'foo', bar: 'bar', baz: 'baz' }).should('deep.equal', {
       foo: 'foo',
       bar: 'bar',
--- cypress/integration/talk/3-debugging.test.js
+++ cypress/integration/talk/3-debugging.test.js
@@ -17,7 +17,6 @@ context('Talk', () => {
 
     cy.get('[data-testid="Your username"]').type('smagni@workwave.com')
     cy.get('[data-testid="Your password"]').type('wrongpassword')
-    cy.pause()
     cy.get('[data-testid="Login"]').click()
 
     cy.contains('The credentials are wrong').should('be.visible')
